138e65f3e56bf63de8a4504577418db83436fdaa
Jimmy Svensson <jimmy888swe@gmail.com>
2020-08-06 23:14:02 +0200

    fix: removed logs and added class to every sq

--- app.js
+++ app.js
@@ -10,17 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // get shuffled game array with random bomb
     const bombArray = Array(bombAmount).fill('bomb');
     const emptyArray = Array(width*width - bombAmount).fill('valid');
-    console.log(bombArray, emptyArray);
     const gameArray = emptyArray.concat(bombArray);
-    console.log(gameArray);
     const shuffledArray = gameArray.sort(()=> Math.random() - 0.5);
-    console.log( shuffledArray );
     
     
     
     for (let i = 0; i < width*width; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', i);
+      square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
     }
