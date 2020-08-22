e3fc5dd2d6a2c2d07f185ae10a9ff6b5b9bd2b3f
Jimmy Svensson <jimmy888swe@gmail.com>
2020-08-23 00:52:33 +0200

    fix: add diff classes with diff bombs

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ function click(square) {
     let total = square.getAttribute('data');
     if (total != 0) {
       square.classList.add('checked');
+      if (total == 1) square.classList.add('one');    
+      if (total == 2) square.classList.add('two');    
+      if (total == 3) square.classList.add('three');    
+      if (total == 4) square.classList.add('four');    
       square.innerHTML = total;
       console.log(total);
       
